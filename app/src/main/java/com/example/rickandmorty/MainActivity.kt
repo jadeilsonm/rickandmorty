fa1698baddbd807310ea6b5393861c8f2f3e4b21
jadeilson <jadeilsonm17@gmail.com>
2025-09-24 23:18:54 -0300

    feat: add fragments

---
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.rickandmorty
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.rickandmorty.databinding.ActivityMainBinding
+import com.example.rickandmorty.fragment.HomeFragment
+import com.example.rickandmorty.fragment.LocationFragment
+import com.example.rickandmorty.fragment.PersonFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+                item -> when (item.itemId) {
+            R.id.id_home -> replaceFragment(HomeFragment())
+            R.id.id_person -> replaceFragment(PersonFragment())
+            R.id.id_location -> replaceFragment(LocationFragment())
+        }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
